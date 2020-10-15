bd574ddebbcd4691351759105d7c5239f8e6e418
breemark <britomaximiliano83@gmail.com>
2020-10-15 17:08:55 +0700

    form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,9 @@
                             <div class="col-auto">
                                 <label>Insert amount to pay</label>
                                 <input type="number" min="5" step="0.01" class="form-control" name="value" value="{{ mt_rand(500, 100000) / 100 }}" required>
+                                <small class="form-text text-muted">
+                                    Use values with up to 2 decimal positions, using a dot "."
+                                </small>
                             </div>
                             <div class="col-auto">
                                 <label>
@@ -26,6 +29,27 @@
                                     </option>
                                     @endforeach
                                 </select>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col">
+                                <label>
+                                    Select the desired payment platform
+                                </label>
+                                <div class="form-group">
+                                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                                        @foreach ($paymentPlatforms as $paymentPlatform)
+                                        <label class="btn btn-outline-secondary rounded m-2 p-1">
+                                            <input 
+                                                type="radio"
+                                                name="payment_platform"
+                                                value="{{ $paymentPlatform->id }}" 
+                                                required>
+                                            <img class="img-thumbnail" src="{{asset($paymentPlatform->image)}}">
+                                        </label>
+                                        @endforeach
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="text-center mt-3">
