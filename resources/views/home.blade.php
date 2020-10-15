86dbb11028d6d24c0ae59b995a187718faf3ab05
breemark <britomaximiliano83@gmail.com>
2020-10-15 15:39:36 +0700

    payment form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,35 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center">{{ __('Make a Payment') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <form action="#" method="POST" id="paymentForm">
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label>Insert amount to pay</label>
+                                <input type="number" min="5" step="0.01" class="form-control" name="value" value="{{ mt_rand(500, 100000) / 100 }}" required>
+                            </div>
+                            <div class="col-auto">
+                                <label>
+                                    Select Currency
+                                </label>
+                                <select class="custom-select" name="currency" required>
+                                    @foreach ($currencies as $currency)
+                                    <option value="{{ $currency->iso }}">
+                                        {{ strtoupper($currency->iso) }}
+                                    </option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="text-center mt-3">
+                            <button type="submit" id="payButton" class="btn btn-outline-primary btn-lg">
+                                Pay
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
