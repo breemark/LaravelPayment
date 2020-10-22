2f84f1ec0b84d0be705251c2f796e0ca60e04663
breemark <britomaximiliano83@gmail.com>
2020-10-22 11:17:10 +0700

    Controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header text-center">{{ __('Make a Payment') }}</div>
 
                 <div class="card-body">
-                    <form action="#" method="POST" id="paymentForm">
+                    <form action="{{ route('pay') }}" method="POST" id="paymentForm">
                         @csrf
                         <div class="row">
                             <div class="col-auto">
@@ -36,19 +36,29 @@
                                 <label>
                                     Select the desired payment platform
                                 </label>
-                                <div class="form-group">
+                                <div class="form-group" id="toggler">
                                     <div class="btn-group btn-group-toggle" data-toggle="buttons">
                                         @foreach ($paymentPlatforms as $paymentPlatform)
-                                        <label class="btn btn-outline-secondary rounded m-2 p-1">
-                                            <input 
-                                                type="radio"
-                                                name="payment_platform"
-                                                value="{{ $paymentPlatform->id }}" 
-                                                required>
+                                        <label 
+                                            class="btn btn-outline-secondary 
+                                            rounded m-2 p-1" 
+                                            data-target="#{{ $paymentPlatform->name }}Collapse" 
+                                            data-toggle="collapse" 
+                                        >
+                                            <input type="radio" name="payment_platform" value="{{ $paymentPlatform->id }}" required>
                                             <img class="img-thumbnail" src="{{asset($paymentPlatform->image)}}">
                                         </label>
                                         @endforeach
                                     </div>
+                                    @foreach ($paymentPlatforms as $paymentPlatform)
+                                    <div
+                                        id="{{ $paymentPlatform->name }}Collapse"
+                                        class="collapse"
+                                        data-parent="#toggler"
+                                    >
+                                        @includeIf('components.'. strtolower($paymentPlatform->name).'-collapse')
+                                    </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
